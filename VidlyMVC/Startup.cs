17abfe741decde23586693b8cc81889111e3852e
Kelvin Abella <abellavinkel@gmail.com>
2018-05-29 14:02:15 +0400

    Add project files.

---
+++ VidlyMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyMVC.Startup))]
+namespace VidlyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
