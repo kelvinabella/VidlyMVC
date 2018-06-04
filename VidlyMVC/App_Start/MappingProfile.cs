8252fd343b7944a25084a623f1420c4b0e8dee1f
Kelvin Abella <abellavinkel@gmail.com>
2018-06-04 14:55:26 +0400

    Set up dto for Api

---
+++ VidlyMVC/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VidlyMVC.Dtos;
+using VidlyMVC.Models;
+
+namespace VidlyMVC.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ VidlyMVC/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VidlyMVC.Models;
+
+namespace VidlyMVC.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
